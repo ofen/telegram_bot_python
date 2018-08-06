7cc5f344b77ddad70c02c2d2cf52d7582e4487d8
ofen <614942+Ofen@users.noreply.github.com>
2018-08-06 23:32:17 +0300

    testing

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ import telebot
 from telebot import apihelper
 import time
 import os
+import time
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_PROXY = os.environ.get('BOT_PROXY')
 bot = telebot.TeleBot(BOT_TOKEN)
-# apihelper.proxy = {'https': 'socks5://127.0.0.1:9050'}
+
+if BOT_PROXY:
+    apihelper.proxy = {'https': 'socks5://127.0.0.1:9050'}
 
 def logging(msg):
     for i in msg:
@@ -14,6 +18,12 @@ def logging(msg):
             print(log_data)
 
 bot.set_update_listener(logging)
+
+@bot.massage_handler(command=['/start'])
+def init(msg):
+    bot.send_message(msg.chat.id, 'Initialization!')
+    time.wait(10)
+    bot.send_message(msg.chat.id, 'Initialization compleat')
 
 @bot.message_handler(regexp='^[Hh]ello$')
 def hello(msg):
